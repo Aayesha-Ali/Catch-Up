bac253335d481a4901e699adc5b777534de5d10c
20108539-AUT <82435505+Seongrok-Shin@users.noreply.github.com>
2022-08-24 13:16:08 +1200

    L

--- Catch-Up/App.js
+++ Catch-Up/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>new</Text>
+      <Text>Hello world!</Text>
       <StatusBar style="auto" />
     </View>
   );
