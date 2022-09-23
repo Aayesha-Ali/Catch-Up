90a24f370971412dd506de84ff8cb2c5d803f291
Aayesha <aayeshali123@gmail.com>
2022-09-23 22:21:15 +1200

    implement home screen

--- Catch-Up/screens/HomeScreen/HomeScreen.js
+++ Catch-Up/screens/HomeScreen/HomeScreen.js
@@ -1,12 +1,41 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity} from 'react-native'
 import React from 'react'
+import styles from './styles';
+import { firebase } from '../../config'
+export default function HomeScreen({navigation}){
+ 
+  const signOutUser = () => firebase.auth().signOut();
+const signoutPress= () => {
+    firebase.auth().signOut() 
+    .then(() => {
+        navigation.navigate('Login')
+    })
+    .catch(e=>{
+     console.error('Sign Out Error', e);
+    });
 
-const HomeScreen = () => {
+    }
+
+    const FriendsList = () => {
+      navigation.navigate('My Friends List')
+  }
+  
   return (
+  
     <View>
       <Text>HomeScreen</Text>
+      <TouchableOpacity
+      style={styles.button}
+      onPress={() => signOutUser()}>
+      <Text style={styles.buttonTitle}>Sign Out</Text>
+  </TouchableOpacity>
+
+  <TouchableOpacity
+      style={styles.button}
+      onPress={() => FriendsList()}>
+      <Text style={styles.buttonTitle}>Friends List</Text>
+  </TouchableOpacity>
+
     </View>
   )
 }
-
-export default HomeScreen
