d230e2016b56a281424604b0688ffc8200b87d7c
Aayesha <aayeshali123@gmail.com>
2022-09-25 11:01:36 +1300

    added error messages to registration page

--- Catch-Up/screens/RegistrationScreen/RegistrationScreen.js
+++ Catch-Up/screens/RegistrationScreen/RegistrationScreen.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import styles from "./styles";
 import { firebase } from "../../config";
+import styles from "./styles";
+//import { db } from "../../config";
 
 export default function RegistrationScreen({ navigation }) {
   const [firstName, setFirstName] = useState("");
@@ -10,43 +11,83 @@ export default function RegistrationScreen({ navigation }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-
+     
   const onFooterLinkPress = () => {
     navigation.navigate("Login");
   };
 
   const onRegisterPress = () => {
-    if (password !== confirmPassword) {
+    if (password.length < 8) {
+      alert("Password must be at least 8characters.")
+      return
+    }
+    else if (password !== confirmPassword) {
       alert("Passwords don't match.")
       return
   }
+  else if (email.length == 0) {
+    alert("Please enter an email address.")
+    return
+  }
+  else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))
+  {
+    alert("Incorrect email format.")
+      return
+  }
 
-  firebase
-      .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then((response) => {
-          const uid = response.user.uid
-          const data = {
-              id: uid,
-              email,
-              firstName,
-              lastName,
-          };
-          const usersRef = firebase.firestore().collection('users')
-          usersRef
-              .doc(uid)
-              .set(data)
-              .then(() => {
-                  navigation.navigate('HomeScreen', {user: data})
-              })
-              .catch((error) => {
-                  alert(error)
-              });
-      })
-      .catch((error) => {
-          alert(error)
-  });
+  else if (firstName.length == 0) {
+    alert("Please enter your first name.")
+    return
+  }
+    else if (!/^[a-z]+$/i.test(firstName))
+    {
+      alert("Incorrect format for first name. You can only use alpanumeric characters")
+        return
+    }
+  
+  else if (lastName.length == 0) {
+    alert("Please enter your last name.")
+    return
+  }
+
+  else if (!/^[a-z]+$/i.test(lastName))
+  {
+    alert("Incorrect format for last name. You can only use alpanumeric characters")
+      return
+  }
+ else {
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then((response) => {
+            const uid = response.user.uid
+            const data = {
+                id: uid,
+                email,
+                firstName,
+                lastName,
+            };
+            const usersRef = firebase.firestore().collection('users')
+            usersRef
+                .doc(uid)
+                .set(data)
+                .then(() => {
+                    navigation.navigate('HomeScreen', {user: data})
+                })
+                .catch((error) => {
+                    alert(error)
+                  
+                
+                });
+        })
+        .catch((error) => {
+         
+          if (error.code === "auth/email-already-in-use") {
+            alert("That email address is already in use!")
+          }
+    });
   };
+}
 
   return (
     <View style={styles.container}>
