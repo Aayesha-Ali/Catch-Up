f174cfc18c990df0cea7b71eb58014383ab1b965
20108539-AUT <82435505+Seongrok-Shin@users.noreply.github.com>
2022-10-24 23:28:00 +1300

    Implement Profile Screen

---
+++ Catch-Up/screens/ProfileScreen/profile.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  SafeAreaView,
+  View,
+  Button,
+  StyleSheet,
+  Image,
+  Text,
+} from "react-native";
+
+export default function profile(props) {
+  //const user = props.route.params.user
+  const user = { firstName: "jiyoung", lastName: "do" };
+
+  return (
+    <SafeAreaView>
+      <View>
+        <View>
+          <Text>
+            {user.firstName} {user.lastName}
+          </Text>
+        </View>
+        <Button
+          title="CHAT"
+          onPress={() => Alert.alert("you can chat to your friend")}
+        />
+        <Button
+          title="MAP"
+          onPress={() => Alert.alert("you can go to map to invite friends")}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
