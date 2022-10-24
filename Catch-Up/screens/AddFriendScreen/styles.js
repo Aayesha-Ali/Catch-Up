3d4503c0bdd17e8313dd80e19aa2f54b01e5c02e
Aayesha <aayeshali123@gmail.com>
2022-10-24 21:20:27 +1300

    edit FriendsScreen

--- Catch-Up/screens/AddFriendScreen/styles.js
+++ Catch-Up/screens/AddFriendScreen/styles.js
@@ -3,43 +3,56 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
+        alignItems: 'center'
+    },
+    title: {
+
+    },
+    logo: {
+        flex: 1,
+        height: 200,
+        width: 300,
+        alignSelf: "center",
+        margin: 30
+    },
+    input: {
+        height: 48,
+        borderRadius: 5,
+        overflow: 'hidden',
         backgroundColor: 'white',
+        marginTop: 10,
+        marginBottom: 10,
+        marginLeft: 30,
+        marginRight: 30,
+        paddingLeft: 16
     },
-    textInput: {
-        margin: 10,
-        backgroundColor: 'lightgrey',
-        padding: 5,
-        borderRadius: 4,
-    },
-    item: {
-        margin: 30,
-        borderBottomWidth: 2,
-        borderBottomColor: "lightgrey",
-        justifyContent: "center",
-      alignItems: "center"
-
-      },
-      username: {
-        fontSize: 25,
-        fontWeight: "bold",
-        marginBottom: 5,
-
-      },
-      
-      name: {
-        fontSize: 16,
-        marginBottom: 5,
-        fontStyle: "italic",
-      },
-      searchbar: {
-        padding: 10,
-        flexDirection: "row",
-        width: "95%",
-        backgroundColor: "#d9dbda",
-        borderRadius: 15,
+    button: {
+        backgroundColor: 'red',
+        marginLeft: 30,
+        marginRight: 30,
+        marginTop: 20,
+        height: 48,
+        borderRadius: 5,
         alignItems: "center",
-        marginBottom: 15
-
-      },
+        justifyContent: 'center'
+    },
+    buttonTitle: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: "bold"
+    },
+    footerView: {
+        flex: 1,
+        alignItems: "center",
+        marginTop: 20
+    },
+    footerText: {
+        fontSize: 16,
+        color: '#2e2e2d'
+    },
+    footerLink: {
+        color: "red",
+        fontWeight: "bold",
+        fontSize: 16
+    }
 })
--- Catch-Up/screens/FriendsScreen/FriendsScreen.js
+++ Catch-Up/screens/FriendsScreen/FriendsScreen.js
@@ -1,89 +1,82 @@
-import {
-  Text,
-  View,
-  StyleSheet,
-  TouchableOpacity,
-  SafeAreaView,
-} from "react-native";
-import React, { useState, useEffect } from "react";
-import { firebase } from "../../config";
-import SearchBar from "../../components/searchBar";
-import List from "../../components/filter";
-import styles from "./styles";
+import { Text, View, StyleSheet, TouchableOpacity, SafeAreaView} from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { firebase } from '../../config';
+import styles from '../LoginScreen/styles';
+import SearchBar from '../../components/searchBar';
+import List from '../../components/filter';
+const FriendsScreen = ({navigation}) => {
 
-const FriendsScreen = ({ navigation }) => {
   const AddFriends = () => {
-    navigation.navigate("Add Friends");
-  };
-  const [searchPhrase, setSearchPhrase] = useState("");
-  const [clicked, setClicked] = useState(false);
-  const [data, setData] = useState([]);
-  const users = firebase.firestore().collection("users");
+    navigation.navigate('Add Friends')
+}
+
+  const FriendRequests = () => {
+    console.log(data);
+    navigation.navigate('Friend Requests', {users: data})
+   
+}
+
+const [searchPhrase, setSearchPhrase] = useState("");
+const [clicked, setClicked] = useState(false);
+const [ data, setData ] = useState([]);
+const users = firebase.firestore().collection('users');
 
   useEffect(() => {
     const loadData = async () => {
-      users.onSnapshot((querySnapshot) => {
-        const data = [];
-        querySnapshot.forEach((doc) => {
-          const { email, firstName, lastName } = doc.data();
-          data.push({
-            id: doc.id,
-            email,
-            firstName,
-            lastName,
-          });
+    users.onSnapshot(
+      querySnapshot => {
+      const data = [];
+      querySnapshot.forEach(doc => {
+        const { username, firstName, lastName } = doc.data();
+        data.push({
+          id: doc.id,
+          username,
+          firstName,
+          lastName,
         });
-        setData(data);
       });
-    };
-    loadData();
+      setData(data);
+    }
+    )
+  };
+  loadData();
   }, []);
-
+ 
   return (
     <View>
-      <TouchableOpacity style={styles.button} onPress={() => AddFriends()}>
-        <Text style={styles.buttonTitle}>Add Friend</Text>
+
+      
+<TouchableOpacity
+          style={styles.button}
+          onPress={() => AddFriends()}>
+          <Text style={styles.buttonTitle}>Add Friend</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+          style={styles.button}
+          onPress={() => FriendRequests()}>
+          <Text style={styles.buttonTitle}>Friend Requests</Text>
       </TouchableOpacity>
 
       <SafeAreaView style={styles.root}>
-        {!clicked}
-        <SearchBar
-          searchPhrase={searchPhrase}
-          setSearchPhrase={setSearchPhrase}
-          clicked={clicked}
-          setClicked={setClicked}
-        />
+      {!clicked}
+      <SearchBar
+        searchPhrase={searchPhrase}
+        setSearchPhrase={setSearchPhrase}
+        clicked={clicked}
+        setClicked={setClicked}
+      />
       </SafeAreaView>
 
-      <List searchPhrase={searchPhrase} data={data} setClicked={setClicked} />
+            <List
+            searchPhrase={searchPhrase}
+            data={data}
+            setClicked={setClicked}
+        
+          />
+
     </View>
-  );
-};
+  )
+      }
+export default FriendsScreen
 
-export default FriendsScreen;
-
-const style = StyleSheet.create({
-  container: {
-    backgroundColor: "lightgrey",
-    padding: 15,
-    borderRadius: 15,
-    margin: 5,
-    marginHorizontal: 10,
-  },
-
-  innerContainer: {
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  email: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  name: {
-    fontSize: 20,
-  },
-  root: {
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
